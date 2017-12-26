dc1f7f4d185997815c70751db1346b969335b892
anatolii stehnii <tsdaemon@gmail.com>
2017-12-26 20:10:18 +0200

    ...

--- apps/codegen.py
+++ apps/codegen.py
@@ -4,7 +4,7 @@ import torch
 import numpy as np
 import random
 import os
-import re
+import sys
 import nltk
 
 import Constants
@@ -51,34 +51,8 @@ def api_codegen_fn():
     result = {}
 
     try:
-        # tokenize text
-        tokens, str_map = tokenize_and_strmap_query(query)
-
-        indices = vocab.convertToIdx(tokens, Constants.UNK_WORD)
-        # TODO: cuda
-        indices_tensor = torch.LongTensor(indices)
-
-        # production model does not use trees
-        cand_list = nn(None, indices_tensor, tokens)
-        candidats = []
-        for cid, cand in enumerate(cand_list[:10]):
-            try:
-                ast_tree = decode_tree_to_python_ast(cand.tree)
-                code = astor.to_source(ast_tree)
-                candidats.append(code)
-            except:
-                logging.debug("Exception in converting tree to code:"
-                              "query: {}, beam pos: {}".format(query, cid))
-
-        if len(candidats) > 0:
-            code = candidats[0]
-            for literal, place_holder in str_map.items():
-                code = code.replace('\'' + place_holder + '\'', literal)
-            result['code'] = code
-        else:
-            logging.error('No code generate for query: {}'.format(query))
-            result['error'] = 'No code was generated for this query.'
-
+        code = gen_code(query)
+        result['code'] = code
     except Exception as e:
         logging.exception('Error occured during code generation for query: {}'.format(query), exc_info=e)
         result['error'] = 'Error occured during code generation for this query.'
@@ -132,6 +106,37 @@ def tokenize_and_strmap_query(query):
 
     return new_query_tokens, str_map
 
+def gen_code(query):
+    # tokenize text
+    tokens, str_map = tokenize_and_strmap_query(query)
+
+    indices = vocab.convertToIdx(tokens, Constants.UNK_WORD)
+    # TODO: cuda
+    indices_tensor = torch.LongTensor(indices)
+
+    # production model does not use trees
+    cand_list = nn(None, indices_tensor, tokens)
+    candidats = []
+    for cid, cand in enumerate(cand_list[:10]):
+        try:
+            ast_tree = decode_tree_to_python_ast(cand.tree)
+            code = astor.to_source(ast_tree)
+            candidats.append(code)
+        except:
+            logging.debug("Exception in converting tree to code:"
+                          "query: {}, beam pos: {}".format(query, cid))
+
+    if len(candidats) > 0:
+        code = candidats[0]
+        for literal, place_holder in str_map.items():
+            code = code.replace('\'' + place_holder + '\'', literal)
+        return code
+    else:
+        raise Exception('No code was generated for query {}.'.format(query))
 
 
-
+# interactive mode
+if __name__ == '__main__':
+    query = sys.argv[1]
+    code = gen_code(query)
+    print("Generated code: {}".format(code))
